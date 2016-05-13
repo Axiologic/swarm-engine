b3169a344ffba8c2addfa8e31a8ecd235b3b529d
Alboaie Sînică <salboaie@gmail.com>
2016-05-13 18:08:28 +0300

    we have the new swarming engine, ready to be improved with better typing checks and use and ready for integration with a global pubsub mechanism

--- tests/swarmPrimitiveTest.js
+++ tests/swarmPrimitiveTest.js
@@ -1,6 +1,8 @@
 
 var flow = require("../lib/index.js");
+
 var f = flow.createSwarm("simpleSwarm", {
+    type:"flow",       // flow, key, contract
     private:{
         a1:"int",
         a2:"int"
@@ -11,19 +13,23 @@ var f = flow.createSwarm("simpleSwarm", {
     begin:function(a1,a2){
         this.a1 = a1;
         this.a2 = a2;
-        this.continue("agent", "doStep", 3, this.afterExecution);
+        this.continue("agent", "doStep", 3);
+        this.wait(this.afterExecution, this, this.keepAlive);
+        //console.log(this.getInnerValue().meta.swarmId);
+    },
+    keepAlive:function(){
+        return false;
     },
     doStep:function(a){
         this.result = this.a1 + this.a2 + a;
-        //this.asyncReturn(null, this.result);
+        this.return(null, this.result);
     },
-    afterExecution: function(err, res){
-        console.log("Execution after switching contexts...");
+    afterExecution: function(err, res, wholeSwarm){
+        //console.log("afterExecution", wholeSwarm);
+        console.log(`this.result should remain undefined and is ${this.result} but res should be 6 and is ${res}`);
+        this.update(wholeSwarm)
+        console.log(`After update this.result should be 6 and is ${this.result} `);
     }
 });
 
-
-f.begin(1,2 );
-setTimeout(function(){
-    console.log(`${f.result} should be 6`);
-}, 2);
+f.begin(1,2);
