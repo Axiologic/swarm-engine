9bff646c23fac26024817a8e628d0e5b360a0fbb
salboaie <salboaie@gmail.com>
2018-03-12 16:33:02 +0200

    refactoring consensus and a design for a simplified PDS version (in memory)

---
+++ modules/ecdsa/lib/sign.js
@@ -0,0 +1,8 @@
+https://stackoverflow.com/questions/39696888/unable-to-sign-a-file-with-nodejs-crypto
+https://kjur.github.io/jsrsasign/
+https://github.com/cryptocoinjs/secp256k1-node
+    https://stackoverflow.com/questions/39499040/generating-ecdsa-signature-with-node-js-crypto
+
+
+
+
