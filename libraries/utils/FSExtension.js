caf79bd55203c67d0bccbd32411a50a2cf8dac0d
asac <asac.cool@gmail.com>
2018-05-09 14:52:05 +0300

    FSExtension update

--- libraries/utils/FSExtension.js
+++ libraries/utils/FSExtension.js
@@ -4,22 +4,50 @@ const os = require("os");
 const child_process = require('child_process');
 const crypto = require('crypto');
 
+// if this is set to true, the logs will be available. Default (false)
+const DEBUG =  process.env.DEPLOYER_DEBUG || false;
+
 function FSExtention(){
 
+    /**
+     * Base path used to resolve all relative paths in the actions bellow.
+     * Default is set to two levels up from the current directory. This can be changed using __setBasePath.
+     * @type {*|string}
+     */
     var basePath = path.join(__dirname, "../../");
 
-    var __resolvePath = function(filename) {
-        if(path.isAbsolute(filename)) {
-            return filename;
-        }
-
-        return path.resolve(basePath, filename);
+    /**
+     * Set the base path to a different absolute directory path.
+     * @param wd {String} absolute directory path.
+     * @private
+     */
+    var __setBasePath = function(wd) {
+        basePath = path.resolve(wd);
     }
 
+    /**
+     * Resolve path into an absolute path. If filePath is relative, the path is resolved using the basePath as first argument.
+     * @param filePath {String} relative or absolute file path.
+     * @returns {String} absolute path
+     * @private
+     */
+    var __resolvePath = function(filePath) {
+        if(path.isAbsolute(filePath)) {
+            return filePath;
+        }
+
+        return path.resolve(basePath, filePath);
+    }
+
+    /**
+     * If the directory structure does not exist, it is created. Like mkdir -p
+     * @param dir {String} dir path
+     * @private
+     */
     var __createDir = function(dir) {
         dir = __resolvePath(dir);
         if (fs.existsSync(dir)) {
-            console.log(dir + " already exist! Continuing ...")
+            log(dir + " already exist! Continuing ...")
             return;
         }
 
@@ -29,37 +57,58 @@ function FSExtention(){
         child_process.execSync(cmd + dir, {stdio:[0,1,2]});
     }
 
-    var __copy = function (src, dest, callback) {
+    /**
+     * Copy a file or directory. The directory can have recursive contents. Like copy -r.
+     * NOTE: If src is a directory it will copy everything inside of the directory, not the entire directory itself.
+     * NOTE: If src is a file, target cannot be a directory.
+     * NOTE: If the destination path structure does not exists, it will be created.
+     * @param src {String} Source file|directory path.
+     * @param dest {String} Destination file|directory path.
+     * @param options {Object} Optional parameters for copy action. Available options:
+     *  - overwrite <Boolean>: overwrite existing file or directory, default is true.
+     *  Note that the copy operation will silently fail if this is set to false and the destination exists.
+     * @param callback {Function}
+     * @private
+     */
+    var __copy = function (src, dest, options, callback) {
         src = __resolvePath(src);
         dest = __resolvePath(dest);
 
-        if (!fs.existsSync(src)) {
-            let msg = `Directory ${src} does not exists! Continuing...`;
-            if(callback) {
-                callback(msg);
-            }
-            return;
-        }
+        callback = callback || function(){};
 
         try{
-            let current = fs.lstatSync(src);
-            if(current.isDirectory()) {
-                __copyDir(src, dest);
-            } else if(current.isFile()) {
-                __copyFile(src, dest);
+            if (!fs.existsSync(src)) {
+                throw  `Source directory or file ${src} does not exists!`;
+            }
+
+            let srcStat = fs.lstatSync(src);
+            if(srcStat.isDirectory()) {
+                __copyDir(src, dest, options);
+            } else if(srcStat.isFile()) {
+                // destination must be a file too
+                __copyFile(src, dest, options);
             }
         } catch (err) {
-            console.error(err);
+            log(err, true);
             callback(err);
             return;
         }
 
-        if(callback) {
-            callback();
-        }
+        callback();
     }
 
-    var __copyDir = function(src, dest) {
+    /**
+     * Copy a directory. The directory can have recursive contents. Like copy -r.
+     * NOTE: Itt will copy everything inside of the directory, not the entire directory itself.
+     * NOTE: If the destination path structure does not exists, it will be created.
+     * @param src {String} Source directory path.
+     * @param dest {String} Destination directory path.
+     * @param options {Object} Optional parameters for copy action. Available options:
+     *  - overwrite <Boolean>: overwrite existing directory, default is true.
+     *  Note that the copy operation will silently fail if this is set to false and the destination exists.
+     * @private
+     */
+    var __copyDir = function(src, dest, options) {
         src = __resolvePath(src);
         dest = __resolvePath(dest);
 
@@ -72,31 +121,58 @@ function FSExtention(){
             let newDest = path.join(dest, files[i]);
 
             if(current.isDirectory()) {
-                // TODO here maybe we save the state before executing copy and if there is a failure, then restore state
-                __rmDir(newDest); // clean copy instead of override
-                __copyDir(newSrc, newDest);
+                __copyDir(newSrc, newDest, options);
             } else if(current.isSymbolicLink()) {
                 var symlink = fs.readlinkSync(newSrc);
                 fs.symlinkSync(symlink, newDest);
             } else {
-                __copyFile(newSrc, newDest);
+                __copyFile(newSrc, newDest, options);
             }
         }
     };
 
-    var __copyFile = function(src, dest) {
+    /**
+     * Copy a file.
+     * NOTE: If src is a file, target cannot be a directory.
+     * NOTE: If the destination path structure does not exists, it will be created.
+     * @param src {String} Source file path.
+     * @param dest {String} Destination file path.
+     * @param options {Object} Optional parameters for copy action. Available options:
+     *  - overwrite <Boolean>: overwrite existing file or directory, default is true.
+     *  Note that the copy operation will silently fail if this is set to false and the destination exists.
+     * @param callback {Function}
+     * @private
+     */
+    var __copyFile = function(src, dest, options) {
         src = __resolvePath(src);
         dest = __resolvePath(dest);
 
+        if(options && options.overwrite === false) {
+            if (fs.existsSync(dest)) {
+                // silently fail if overwrite is set to false and the destination exists.
+                let error = `Silent fail - cannot copy. Destination file ${dest} already exists and overwrite option is set to false! Continuing...`;
+                log(error, true);
+                return;
+            }
+        }
         __createDir(path.dirname(dest));
 
         var content = fs.readFileSync(src, "utf8");
         fs.writeFileSync(dest, content);
     }
 
+    /**
+     * Removes a file or directory. The directory can have recursive contents. Like rm -rf
+     * @param src {String} Path
+     * @param callback {Function}
+     * @private
+     */
     var __remove = function(src, callback) {
         src = __resolvePath(src);
-        console.log(`Removing ${src}`);
+
+        callback = callback || function(){};
+
+        log(`Removing ${src}`);
 
         try{
             let current = fs.lstatSync(src);
@@ -106,21 +182,24 @@ function FSExtention(){
                 __rmFile(src);
             }
         } catch (err) {
-            console.error(err);
-            callback(err);
+            log(err, true);
+            callback();
             return;
         }
 
-        if(callback) {
-            callback();
-        }
+        callback();
     }
 
+    /**
+     * Removes a directory. The directory can have recursive contents. Like rm -rf
+     * @param dir {String} Path
+     * @private
+     */
     var __rmDir = function (dir) {
         dir = __resolvePath(dir);
 
         if (!fs.existsSync(dir)) {
-            console.error(`Directory ${dir} does not exist!`);
+            log(`Directory ${dir} does not exist!`, true);
             return;
         }
 
@@ -140,41 +219,79 @@ function FSExtention(){
         fs.rmdirSync(dir);
     }
 
+    /**
+     * Removes a file.
+     * @param file {String} Path
+     * @private
+     */
     var __rmFile = function(file) {
         file = __resolvePath(file);
         if (!fs.existsSync(file)) {
-            console.error(`File ${file} does not exist!`);
+            log(`File ${file} does not exist!`, true);
             return;
         }
 
         fs.unlinkSync(file);
     }
 
-
-    var __createFile = function(file, content) {
+    /**
+     * Writes data to a file, replacing the file if it already exists.
+     * @param file {String} Path.
+     * @param data {String}
+     * @private
+     */
+    var __createFile = function(file, data, options) {
         file = __resolvePath(file)
-        fs.writeFileSync(file, content);
+        fs.writeFileSync(file, data, options);
     }
 
-    var __move = function(src, dest, callback) {
+    /**
+     * Moves a file or directory.
+     * @param src {String} Source path.
+     * @param dest {String} Destination path.
+     * @param options {Object}. Optional parameters for copy action. Available options:
+     *  - overwrite <boolean>: overwrite existing file or directory, default is false. Note that the move operation will silently fail if you set this to true and the destination exists.
+     * @param callback {Function}
+     * @private
+     */
+    var __move = function(src, dest, options, callback) {
         src = __resolvePath(src);
         dest = __resolvePath(dest);
 
+        callback = callback || function(){};
+
         try {
-            __rmDir(dest);
-            __copyDir(src, dest);
-            __rmDir(src);
-        } catch(err) {
-            if(callback) {
-                callback(err);
+            if(options && options.overwrite === false) {
+                if (fs.existsSync(dest)) {
+                    // silently fail if overwrite is set to false and the destination exists.
+                    let error = `Silent fail - cannot move. Destination file ${dest} already exists and overwrite option is set to false! Continuing...`;
+                    log(error, true);
+                    callback();
+                    return;
+                }
             }
+
+            __copy(src, dest, options);
+            __remove(src)
+        } catch(err) {
+            callback(err);
+            return;
         }
 
-        if(callback) {
-            callback();
-        }
+        callback();
     }
 
+    /**
+     * Computes checksum to a file or a directory based on their contents only.
+     * If the source is directory, the checksum is a hash of all concatenated file hashes.
+     * @param src {String} Path of a file or directory.
+     * @param algorithm {String} Hashing algorithm(default: md5). The algorithm is dependent on the available algorithms
+     * supported by the version of OpenSSL on the platform. E.g. 'md5', 'sha256', 'sha512'.
+     * @param encoding {String} Hashing encoding (default: 'hex'). The encoding is dependent on the
+     * available digest algorithms. E.g. 'hex', 'latin1' or 'base64'.
+     * @returns {String} Checksum of the file or directory.
+     * @private
+     */
     var __checksum = function(src, algorithm, encoding) {
         src = __resolvePath(src);
 
@@ -194,6 +311,16 @@ function FSExtention(){
         return checksum;
     }
 
+    /**
+     * Computes hash of a string.
+     * @param str {String}
+     * @param algorithm {String} Hashing algorithm(default: md5). The algorithm is dependent on the available algorithms
+     * supported by the version of OpenSSL on the platform. E.g. 'md5', 'sha256', 'sha512'.
+     * @param encoding {String} Hashing encoding (default: 'hex'). The encoding is dependent on the
+     * available digest algorithms. E.g. 'hex', 'latin1' or 'base64'.
+     * @returns {String} Hash of the string.
+     * @private
+     */
     var __hash =  function(str, algorithm, encoding) {
         return crypto
             .createHash(algorithm || 'md5')
@@ -201,6 +328,16 @@ function FSExtention(){
             .digest(encoding || 'hex')
     }
 
+    /**
+     * Computes hash of a file based on its content only.
+     * @param src {String} Path of a file.
+     * @param algorithm {String} Hashing algorithm(default: md5). The algorithm is dependent on the available algorithms
+     * supported by the version of OpenSSL on the platform. E.g. 'md5', 'sha256', 'sha512'.
+     * @param encoding {String} Hashing encoding (default: 'hex'). The encoding is dependent on the
+     * available digest algorithms. E.g. 'hex', 'latin1' or 'base64'.
+     * @returns {String} Hash of the file.
+     * @private
+     */
     var __hashFile = function(src, algorithm, encoding) {
         src = __resolvePath(src);
         if (!fs.existsSync(src)) {
@@ -211,6 +348,17 @@ function FSExtention(){
         return __hash(content, algorithm, encoding);
     }
 
+    /**
+     * Computes hash of a directory based on its content only.
+     * If directory has multiple files, the result is a hash of all concatenated file hashes.
+     * @param src {String} Path of a directory.
+     * @param algorithm {String} Hashing algorithm(default: md5). The algorithm is dependent on the available algorithms
+     * supported by the version of OpenSSL on the platform. E.g. 'md5', 'sha256', 'sha512'.
+     * @param encoding {String} Hashing encoding (default: 'hex'). The encoding is dependent on the
+     * available digest algorithms. E.g. 'hex', 'latin1' or 'base64'.
+     * @returns {String} Hash of the directory.
+     * @private
+     */
     var __hashDir = function(dir, algorithm, encoding) {
         dir = __resolvePath(dir);
         if (!fs.existsSync(dir)) {
@@ -244,7 +392,34 @@ function FSExtention(){
         }
     }
 
+    /**
+     * Generates a guid (global unique identifier).
+     * @returns {String} Guid in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+     * @private
+     */
+    var __guid = function guid() {
+        function _make_group(s) {
+            var p = (Math.random().toString(16)+"000000000").substr(2,8);
+            return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+        }
+        return _make_group() + _make_group(true) + _make_group(true) + _make_group();
+    }
+
+    /**
+     * Logs wrapper.
+     * @param message {String}
+     * @param isError {Boolean}
+     */
+    function log(message, isError) {
+        let logger = isError ? console.error : console.log;
+
+        if(DEBUG) {
+            logger(message);
+        }
+    }
+
     return {
+        setBasePath: __setBasePath,
         resolvePath: __resolvePath,
         createDir: __createDir,
         copyDir: __copyDir,
@@ -254,7 +429,8 @@ function FSExtention(){
         copy: __copy,
         move: __move,
         remove: __remove,
-        checksum: __checksum
+        checksum: __checksum,
+        guid: __guid
     }
 }
 
