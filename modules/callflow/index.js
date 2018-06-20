e24ed2bdd0e9f1f5d482b6912359fa00a5fde115
asac <asac.cool@gmail.com>
2018-06-20 15:09:40 +0300

    fixing loadLibrary

--- modules/callflow/index.js
+++ modules/callflow/index.js
@@ -68,8 +68,8 @@ $$.PSK_PubSub = require("./lib/soundPubSub");
 
 module.exports = {
     				createSwarmEngine: require("./lib/swarmDescription").createSwarmEngine,
-					createJoinPoint: require("./lib/parallelJoinPoint").createJoinPoint,
-					createSerialJoinPoint: require("./lib/serialJoinPoint").createSerialJoinPoint,
+                    createJoinPoint: require("./lib/parallelJoinPoint").createJoinPoint,
+                    createSerialJoinPoint: require("./lib/serialJoinPoint").createSerialJoinPoint,
 					"safe-uuid": require("./lib/safe-uuid"),
 					beesHealer: require("./lib/beesHealer")
 				};
