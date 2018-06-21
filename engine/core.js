1f89f5e2c451d5ec0c70e775b03dc4dfe511bd76
asac <asac.cool@gmail.com>
2018-06-21 17:03:22 +0300

    fixing requireModule

--- engine/core.js
+++ engine/core.js
@@ -1,118 +1,21 @@
 /*
-Initial License: (c) Axiologic Research & Alboaie Sînică.
-Contributors: Axiologic Research , PrivateSky project
-Code License: LGPL or MIT.
-*/
+ Initial License: (c) Axiologic Research & Alboaie Sînică.
+ Contributors: Axiologic Research , PrivateSky project
+ Code License: LGPL or MIT.
+ */
 
 var path = require("path");
+var callflowModule = require("./../modules/callflow");
 
-function defaultErrorHandlingImplementation(err, res){
-    //console.log(err.stack);
-    if(err) throw err;
-    return res;
-}
+$$.securityContext = "system";
+$$.libraryPrefix = "global";
+$$.libraries = {
+    global:{
 
-$$ = {
-    errorHandler: {
-        error:function(err, args, msg){
-            console.log(err, "Unknown error from function call with arguments:", args, "Message:", msg);
-        },
-        throwError:function(err, args, msg){
-            console.log(err, "Unknown error from function call with arguments:", args, "Message:", msg);
-            throw err;
-        },
-        ignorePossibleError: function(name){
-            console.log(name);
-        },
-        syntaxError:function(property, swarm, text){
-            //throw new Error("Misspelled member name or other internal error!");
-            var swarmName;
-            try{
-                if(typeof swarm == "string"){
-                    swarmName = swarm;
-                } else
-                if(swarm && swarm.meta){
-                    swarmName  = swarm.meta.swarmTypeName;
-                } else {
-                    swarmName = swarm.getInnerValue().meta.swarmTypeName;
-                }
-            } catch(err){
-                swarmName = err.toString();
-            }
-            if(property){
-                console.log("Wrong member name ", property,  " in swarm ", swarmName);
-                if(text) {
-                    console.log(text);
-                }
-            } else {
-                console.log("Unknown swarm", swarmName);
-            }
-
-        },
-        warning:function(msg){
-            console.log(msg);
-        }
-    },
-    securityContext:"system",
-    uidGenerator: require("./choreographies/safe-uuid.js"),
-    safeErrorHandling:function(callback){
-        if(callback){
-            return callback;
-        } else{
-            return defaultErrorHandlingImplementation;
-        }
-    },
-    libraryPrefix:"global",
-    libraries: {
-        global:{
-
-        }
-    },
-    defaultErrorHandlingImplementation:defaultErrorHandlingImplementation,
-    __intern:{
-        mkArgs:function(args,pos){
-            var argsArray = [];
-            for(var i = pos; i < args.length; i++){
-                argsArray.push(args[i]);
-            }
-            return argsArray;
-        }
     }
 };
 
-$$.registerSwarmDescription =  function(libraryName,shortName, description){
-    if(!$$.libraries[libraryName]){
-        $$.libraries[libraryName] = {};
-    }
-    $$.libraries[libraryName][shortName] = description;
-}
-
-
-var swarmDescr = require("./choreographies/swarmDescription");
-
-
-$$.callflows        = swarmDescr.createSwarmEngine("callflow");
-$$.callflow         = $$.callflows;
-$$.flow             = $$.callflows;
-$$.flows            = $$.callflows;
-$$.swarms           = swarmDescr.createSwarmEngine("swarm");
-$$.swarm            = $$.swarms;
-$$.contracts        = swarmDescr.createSwarmEngine("contract");
-$$.contract         = $$.contracts;
-
-$$.loadLibrary      = require("./util/loadLibrary").loadLibrary;
-
-exports.enableTesting = function() {
-    //$$.PSK_PubSub = require("./pubSub/InternalPubSub").internalBus;
-    $$.PSK_PubSub = require("./pubSub/core/soundPubSub").soundPubSub;  //for testing
-
-    require("./fakes/dummyVM");
-    return exports;
-}
-
-var loadedModules = {
-
-}
+var loadedModules = {};
 
 $$.requireModule = function(name){
     var existingModule = loadedModules[name];
@@ -122,13 +25,34 @@ $$.requireModule = function(name){
         loadedModules[name] = existingModule;
     }
     return existingModule;
-}
+};
 
 $$.requireLibrary = function(name){
     var absolutePath = path.resolve( __dirname + "/../libraries/" + name);
     return $$.loadLibrary(name,absolutePath);
+};
+
+$$.registerSwarmDescription =  function(libraryName,shortName, description){
+    if(!$$.libraries[libraryName]){
+        $$.libraries[libraryName] = {};
+    }
+    $$.libraries[libraryName][shortName] = description;
 }
 
+var utils = require("./choreographies/utilityFunctions");
+
+$$.swarms           = callflowModule.createSwarmEngine("swarm", utils);
+$$.swarm            = $$.swarms;
+$$.contracts        = callflowModule.createSwarmEngine("contract", utils);
+$$.contract         = $$.contracts;
+
+$$.loadLibrary      = require("./util/loadLibrary").loadLibrary;
+
+require("./choreographies/swarmInstancesManager");
+
+exports.enableTesting = function() {
+    require("./fakes/dummyVM");
+}
 
 var core = $$.requireLibrary("core");
 
@@ -137,16 +61,10 @@ $$.ensureFolderExists = function(folder, callback){
 
     var flow = $$.flow.start(core.mkDirRec);
     flow.make(folder, callback);
-
-}
-
+};
 
 $$.ensureLinkExists = function(existingPath, newPath, callback){
 
     var flow = $$.flow.start(core.mkDirRec);
     flow.makeLink(existingPath, newPath, callback);
-}
-
-
-
-
+};
