45ced1cc62ce2f0fe957f52d356546886cbd6195
salboaie <salboaie@gmail.com>
2018-06-28 18:13:45 +0300

    smart contractions vision

--- libraries/domain/domain.js
+++ libraries/domain/domain.js
@@ -1,19 +1,34 @@
-$$.contract.describe("domain", {
+$$.contract.describe("domainConstitution", {
     public:{
+        pdsInstance: "native",
         shares      :"contract",
         agents      :"contract",
         stakeHolders:"contract"
     },
-    create:function(ammount){
+    create:function(pdsInstance){
+    },
+    getContract:function(name){
+
+    },
+    saveTransaction:function(){
+
     }
 });
 
+var domainReplicaAPIs = $$.contract.create("domainConstitution");
 
 $$.transaction.describe("domainTransaction", {
     public:{
         contractInstace:"contract"
     },
     create:function(contractName, action, args){
-        currentDomain[contractName][action].apply(args);
+
+        var smc = domainReplicaAPIs.getContract(contractName);
+        smc.transaction = this;
+        smc.domain = currentDomain;
+        smc[action].apply(args);
+    },
+    save:function(){
+        domainReplicaAPIs.saveTransaction(this);
     }
 });
