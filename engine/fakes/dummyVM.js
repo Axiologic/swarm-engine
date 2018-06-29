bee1f300e6ca53ed2f034c2c857406f2080fcd44
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2018-06-29 18:15:19 +0300

    restored dummyVM for now

---
+++ engine/fakes/dummyVM.js
@@ -0,0 +1,10 @@
+function dummyVM(name){
+	function solveSwarm(swarm){
+		$$.swarmsInstancesManager.revive_swarm(swarm);
+	}
+
+	$$.PSK_PubSub.subscribe(name, solveSwarm);
+	console.log("Creating a fake execution context...");
+}
+
+vm = dummyVM($$.CONSTANTS.SWARM_FOR_EXECUTION);
