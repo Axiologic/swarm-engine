e97f92f178473b121452c7a650c9f4bcd4c9e172
salboaie <master1!23>
2018-07-18 11:12:07 +0300

    client APIs with fake http server

--- libraries/localNode/localNode.js
+++ libraries/localNode/localNode.js
@@ -1,6 +1,6 @@
 /* define functions for working with the local node*/
 
-var ss = requireModule("signsensus");
+var ss = $$.requireModule("signsensus");
 
 $$.flow.describe("localNode", {
     public:{
@@ -17,6 +17,9 @@ $$.flow.describe("localNode", {
             }
         });
 
+    },
+    canExecuteCodeForAgent: function(agentUrl, swarm, signature){
+        return true;
     },
     getAgentsForDomain:function(domain, callflow){        //give the list of replication agents for a domain
         var d = domainCache[d];
