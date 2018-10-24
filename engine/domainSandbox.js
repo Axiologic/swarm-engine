49a3130e0a4b6b298ed8c46cc69746deb43482c2
salboaie <master1!23>
2018-10-24 16:05:02 +0300

    small fix

--- engine/domainSandbox.js
+++ engine/domainSandbox.js
@@ -36,8 +36,8 @@ $$.remote.newEndPoint('virtualmqEndpoint', `${config.endpoint}:${config.port}`, 
 
 $$.remote.virtualmqEndpoint.on('*', '*', function (err, res) {
     $$.PSK_PubSub.subscribe($$.CONSTANTS.SWARM_RETURN, (swarm) => {
-        if(swarm && swarm.meta && swarm.meta.returnChannel){
-        $$.remote.doHttpPost(swarm.meta.returnChannel, swarm, function(err, res){
+        if(swarm && swarm.meta && swarm.meta.homeSecurityContext){
+        $$.remote.doHttpPost(swarm.meta.homeSecurityContext, swarm, function(err, res){
             if(err){
                 console.log(err);
             }
