94f48366860b2ef4ae7781edf448e089525e9c86
skutner <sava.dumitru.daniel@gmail.com>
2019-02-18 13:29:47 +0200

    removed busboy dependency

--- bin/scripts/build-devel.js
+++ bin/scripts/build-devel.js
@@ -132,7 +132,7 @@ const config = {
             ]
         },
         {
-            "name": "browserify chokidar busboy",
+            "name": "browserify chokidar",
             "src": "npm",
             "actions": ["install"]
         },
