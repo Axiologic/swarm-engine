776baed91454eefe70ff5447f7ea552bd1ff4004
asac <asac.cool@gmail.com>
2019-07-18 16:19:57 +0300

    delayed restart in case of errors for virtualmq and launcher processes

--- psknode/bin/scripts/psknode.js
+++ psknode/bin/scripts/psknode.js
@@ -5,6 +5,8 @@
  */
 
 const {spawnSync, fork, spawn} = require('child_process');
+const max_timeout = 10*60*1000; // 10 minutes
+const restartDelays = {};
 
 let shouldRestart = true;
 const forkedProcesses = {};
@@ -16,11 +18,21 @@ function startProcess(filePath) {
 
     console.log('SPAWNED ', forkedProcesses[filePath].pid);
 
+    function restartWithDelay(filePath){
+        let timeout = restartDelays[filePath] || 100;
+        console.log(`Process will restart in ${timeout} ms ...`);
+        setTimeout(()=>{
+            restartDelays[filePath] = (timeout * 2) % max_timeout;
+            startProcess(filePath);
+        }, timeout);
+    }
+
     function errorHandler(filePath) {
+        let timeout = 100;
         return function (error) {
             console.log(`\x1b[31mException caught on spawning file ${filePath} `, error ? error : "", "\x1b[0m"); //last string is to reset terminal colours
             if (shouldRestart) {
-                startProcess(filePath);
+                restartWithDelay(filePath);
             }
         }
     }
@@ -29,7 +41,7 @@ function startProcess(filePath) {
         return function () {
             console.log(`\x1b[33mExit caught on spawned file ${filePath}`, "\x1b[0m"); //last string is to reset terminal colours
             if (shouldRestart) {
-                startProcess(filePath);
+                restartWithDelay(filePath);
             }
         }
     }
