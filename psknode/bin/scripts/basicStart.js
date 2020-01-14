cfa13414ccbda7b0a7867e4fcaf4ff540746a105
asaccool <asac.cool@gmail.com>
2020-01-14 11:19:12 +0200

    startup helper

--- psknode/bin/scripts/basicStart.js
+++ psknode/bin/scripts/basicStart.js
@@ -4,5 +4,5 @@ if(process.argv.length < 3){
     console.log("Wrong usage. This script enables fork of process received as param no.3");
     process.exit(1);
 }
-const forkExtraArgs = process.argv.slice(2);
+const forkExtraArgs = process.argv.slice(3);
 pingFork(process.argv[2], forkExtraArgs);
