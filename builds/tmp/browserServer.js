3ad5c4ec2f2b52eca1a9c515f7b17686f65c09ba
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-01-22 18:36:20 +0200

    browser bundles update

--- builds/tmp/browserServer.js
+++ builds/tmp/browserServer.js
@@ -3,6 +3,4 @@ if(typeof $$ === "undefined"){
 }
 $$.browserRuntime = true;
 require("../../modules/overwrite-require");
-require("../../modules/callflow/standardGlobalSymbols");
-require("./swBrowserified_intermediar");
-
+require("./browserServer_intermediar");
