0325b30fad48db727b71377cb84be5f3c75e8bdb
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-28 14:29:50 +0200

    Cleanup build.json

---
+++ builds/tmp/domainBoot.js
@@ -0,0 +1,5 @@
+const or = require('overwrite-require');
+or.enableForEnvironment(or.constants.NODEJS_ENVIRONMENT_TYPE);
+require("../../modules/callflow/standardGlobalSymbols");
+
+require("./domainBoot_intermediar");
