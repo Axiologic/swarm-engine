89682abaa0bbc2843482a00ff4e7f709053989ff
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-03-27 15:21:07 +0200

    removed temporary browser logger fix

--- builds/tmp/swHostBoot.js
+++ builds/tmp/swHostBoot.js
@@ -1,4 +1,3 @@
 const or = require('overwrite-require');
 or.enableForEnvironment(or.constants.SERVICE_WORKER_ENVIRONMENT_TYPE);
-$$.log = $$.err = $$.fixMe = console.log;
 require("./swHostBoot_intermediar");
