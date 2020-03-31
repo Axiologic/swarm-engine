4987bd1404b08bc91708cf7c679a26e7601e02fc
PSK Build Tracker <psk.build.track@gmail.com>
2020-03-31 10:36:42 +0300

    travis-ci script

---
+++ .travis/push_test_report.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "psk.build.track@gmail.com"
+  git config --global user.name "PSK Build Tracker"
+}
+
+commit_test_report() {
+  git checkout -b test_reports
+  git add tests/psk-smoke-testing/testReport.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+push_to_github() {
+  git remote add new_origin https://${GIT_TOKEN}@github.com/privatesky/privatesky > /dev/null 2>&1
+  git push --quiet --set-upstream  new_origin test_reports
+}
+
+setup_git
+commit_test_report
+push_to_github
